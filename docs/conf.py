a3611507f56b59e8c79d70f37c79870789937766
Daisy Harris <dasharon@byu.edu>
2023-12-07 13:35:37 -0700

    gh-pages

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ source_suffix = ['.rst', '.md']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+
 import sphinx_bootstrap_theme
 html_theme = 'bootstrap'
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
