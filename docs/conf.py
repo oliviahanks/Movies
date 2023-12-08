f86cd6137d28135d4e4c065f4d868963ba460b3f
Daisy Harris <dasharon@byu.edu>
2023-12-08 09:06:08 -0700

    Update theme

--- docs/conf.py
+++ docs/conf.py
@@ -48,9 +48,9 @@ source_suffix = ['.rst', '.md']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-import sphinx_bootstrap_theme
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+# import sphinx_bootstrap_theme
+html_theme = 'cloud'
+# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
